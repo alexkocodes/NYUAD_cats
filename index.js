93388f1b7f71254f3ae07414e9935eed22efb5d2
Alex Ko <fyk211@nyu.edu>
2022-02-21 01:38:31 +0400

    added sounds

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
+let click = 0;
 document.getElementById("cat1").addEventListener("click", function(e){
-    document.body.style.backgroundColor = "#87d6e5"
+    document.body.style.backgroundColor = "#87d6e5";
+    click += 1;
+    //console.log(click);
+    if(click >= 5){
+        alert("Don't play with us too much...");
+        click = 0;
+    }
 });
 document.getElementById("cat2").addEventListener("click", function(e){
-    document.body.style.backgroundColor = "#fad788"
+    document.body.style.backgroundColor = "#fad788";
+    click += 1;
+    if(click >= 5){
+        alert("Don't play with us too much...");
+        click = 0;
+    }
 });
 document.getElementById("cat3").addEventListener("click", function(e){
-    document.body.style.backgroundColor = "#ffc4c4"
+    document.body.style.backgroundColor = "#ffc4c4";
+    click += 1;
+    //console.log(click);
+    if(click >= 5){
+        alert("Don't play with us too much...");
+        click = 0;
+    }
 });
 
+
 let meow = new Audio('./sounds/meow.wav');
+let meow2 = new Audio('./sounds/meow2.wav');
+let meow3 = new Audio('./sounds/meow3.wav');
+all_meows = [meow, meow2, meow3];
 let cats = document.getElementsByClassName("cat");
 for (let i=0; i<cats.length; i++){
     cats[i].addEventListener("click", function(event){
-        meow.play();
+        all_meows[i].play();
     })
 }
 
@@ -38,10 +60,6 @@ elements.forEach(element => {
 });
 
 
-
-
-
-// for presentation purpose
 setTimeout(() => {
   elements.forEach(element => {
     element.classList.add('play');
